971081e25310ce2f940e8462cfa514207f69ebcc
XadillaX <admin@xcoder.in>
2015-01-27 00:39:30 +0800

    the new look

---
+++ src/scripts/base.js
@@ -0,0 +1,43 @@
+/**
+ * XadillaX created at 2015-01-26 21:50:17
+ *
+ * Copyright (c) 2015 Huaban.com, all rights
+ * reserved
+ */
+if(typeof $ === "undefined") {
+    var $, document, window;
+}
+
+var gui = require("nw.gui");
+var win = gui.Window.get();
+var boardThick;
+var titleThick;
+
+function resizeBorder(w, h) {
+    var trueHeight = h - titleThick - boardThick;
+    $(".whole-v-border").css("height", trueHeight + "px");
+    $(".bottom-border").css("width", w + "px");
+    $(".main-frame").css({ height: trueHeight + "px", width: (w - 2 * boardThick) + "px" });
+}
+
+$(function() {
+    boardThick = $(".whole-v-border").width();
+    titleThick = $(".whole-title-bar").height();
+
+    resizeBorder(win.width, win.height);
+
+    win.on("resize", function(w, h) {
+        resizeBorder(w, h);
+    });
+
+    $(document).keypress(function(e) {
+        if(e.ctrlKey && e.keyCode === 4) {
+            win.showDevTools();
+        }
+
+        if(e.ctrlKey && e.keyCode === 18) {
+            window.location.reload();
+        }
+    });
+});
+
